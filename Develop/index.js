ceea7b2fbdb33dd68d7ff32e7b8b853576073d22
Emily Crook <Emily.A.Crook96@gmail.com>
2021-04-24 21:38:55 -0500

    start of needed added files

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,15 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const generateMarkdown = require ('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data), err => {
+    return `
+    
+    `
+}
 
 // TODO: Create a function to initialize app
 function init() {}
